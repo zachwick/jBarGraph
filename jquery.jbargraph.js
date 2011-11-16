de18210e143ee8feee6c3816fad661609f22a0b3
Zach Wick <zachary.wick@gmail.com>
2011-11-15 23:56:19 -0500

    Added vertical bar graphs functionality

--- jquery.jbargraph.js
+++ jquery.jbargraph.js
@@ -66,23 +66,24 @@ jQuery.fn.bar_graph = function(options) {
     }
     var percentArray = new Array();
     var dataMax = options.data.max();
-    for (var i=0;i<options.data.length;i++) {
-        percentArray[i] = (options.data[i] / dataMax) * jQuery(this).width();
-    }
-    if (options.style = 'horizontal') {
+
+    if (options.style == 'horizontal') {
+        for (var i=0;i<options.data.length;i++) {
+            percentArray[i] = (options.data[i] / dataMax) * jQuery(this).width();
+        }
 	var barH = (jQuery(this).height() / options.data.length -5);
         for (var i=0;i<options.data.length;i++) {
 	    jQuery(this).append("<div class='hbar-chart-bar' id='hbar-"+i+"'></div>");
-            jQuery("#hbar-"+i).width(percentArray[i]);
-            jQuery("#hbar-"+i).height(barH);
+            jQuery(this).children("#hbar-"+i).width(percentArray[i]);
+            jQuery(this).children("#hbar-"+i).height(barH);
 	    if (i in correctArray) {
-		jQuery("#hbar-"+i).css('background-color',options.colors[0]);
+		jQuery(this).children("#hbar-"+i).css('background-color',options.colors[0]);
 	    } else {
-		jQuery("#hbar-"+i).css('background-color',options.colors[1]);
+		jQuery(this).children("#hbar-"+i).css('background-color',options.colors[1]);
 	    }
 	    if (options.barStyle == 'fancy') {
-		jQuery("#hbar-"+i).css("border-top-right-radius",barH/4);
-		jQuery("#hbar-"+i).css("border-bottom-right-radius",barH/4);
+		jQuery(this).children("#hbar-"+i).css("border-top-right-radius",barH/4);
+		jQuery(this).children("#hbar-"+i).css("border-bottom-right-radius",barH/4);
 	    }
 	    /*if (jQuery("#hbar-"+i).prev().hasClass('hbar-chart-bar-label')) {
 		jQuery("#hbar-"+i).css('top',-1*(jQuery("#hbar-"+i).prev().height()));
@@ -90,43 +91,98 @@ jQuery.fn.bar_graph = function(options) {
 
 	    if (options.labelStyle == 'text') {
 		if (options.labelDisplay == 'static') {
-		    jQuery("#hbar-"+i).append("<div class='hbar-chart-bar-label' id='hlabel-"+i+"'>"+options.labels[i]+"</div>");
+		    jQuery(this).children("#hbar-"+i).append("<div class='hbar-chart-bar-label' id='hlabel-"+i+"'>"+options.labels[i]+"</div>");
 		} else if (options.labelDisplay == 'hover') {
-	            jQuery("#hbar-"+i).attr('title',options.labels[i]);
+	            jQuery(this).children("#hbar-"+i).attr('title',options.labels[i]);
 		} else if (options.labelDisplay == 'scale') {
-		    jQuery("#hbar-"+i).append("<div class='hbar-chart-bar-label' id='hlabel-"+i+"'>"+options.labels[i]+"</div>");
-		    jQuery("#hlabel-"+i).css('font-size',barH/3);
+		    jQuery(this).children("#hbar-"+i).append("<div class='hbar-chart-bar-label' id='hlabel-"+i+"'>"+options.labels[i]+"</div>");
+		    jQuery(this).children("#hlabel-"+i).css('font-size',barH/3);
 		} else {
 			console.log("labelDisplay with value '"+options.labelDisplay+"' is meaningless");
 		}
 	    } else if (options.labelStyle == 'value') {
 		if (options.labelDisplay == 'static') {
-			jQuery("#hbar-"+i).append("<div class='hbar-chart-bar-label' id='hlabel-"+i+"'>"+options.data[i]+"</div>"); 
+		    jQuery(this).children("#hbar-"+i).append("<div class='hbar-chart-bar-label' id='hlabel-"+i+"'>"+options.data[i]+"</div>"); 
 		} else if (options.labelDisplay == 'hover') {
-			jQuery("#hbar-"+i).attr('title',options.data[i]);
+		    jQuery(this).children("#hbar-"+i).attr('title',options.data[i]);
 		} else if (options.labelDisplay == 'scale') {
-		    jQuery("#hbar-"+i).append("<div class='hbar-chart-bar-label' id='hlabel-"+i+"'>"+options.data[i]+"</div>");
-		    jQuery("#hlabel-"+i).css('font-size',barH/3);
+		    jQuery(this).children("#hbar-"+i).append("<div class='hbar-chart-bar-label' id='hlabel-"+i+"'>"+options.data[i]+"</div>");
+		    jQuery(this).children("#hlabel-"+i).css('font-size',barH/3);
 		} else {
 			console.log("labelDisplay with value '"+options.labelDisplay+"' is meaningless");
 		}
 	    } else if (options.labelStyle == 'full') {
 		if (options.labelDisplay == 'static') {
-			jQuery("#hbar-"+i).append("<div class='hbar-chart-bar-label' id='hlabel-"+i+"'>"+options.labels[i]+" ("+options.data[i]+")</div>");
+		    jQuery(this).children("#hbar-"+i).append("<div class='hbar-chart-bar-label' id='hlabel-"+i+"'>"+options.labels[i]+" ("+options.data[i]+")</div>");
 		} else if (options.labelDisplay == 'hover') {
-			jQuery("#hbar-"+i).attr('title',options.labels[i]+" ("+options.data[i]+")");
+		    jQuery(this).children("#hbar-"+i).attr('title',options.labels[i]+" ("+options.data[i]+")");
 		} else if (options.labelDisplay == 'scale') {
-		    jQuery("#hbar-"+i).append("<div class='hbar-chart-bar-label' id='hlabel-"+i+"'>"+options.labels[i]+" ("+options.data[i]+")</div>");
-		    jQuery("#hlabel-"+i).css('font-size',barH/3);
-		} else {
+		    jQuery(this).children("#hbar-"+i).append("<div class='hbar-chart-bar-label' id='hlabel-"+i+"'>"+options.labels[i]+" ("+options.data[i]+")</div>");
+		    jQuery(this).children("#hlabel-"+i).css('font-size',barH/3);
+		} else { 
 			console.log("labelDisplay with value '"+options.labelDisplay+"' is meaningless");
 		}
 	    } else {
 		console.log("labelStyle with value '"+options.labelStyle+"' is meaningless");
 	    }
         }
-    } else if (options.style = 'vertical') {
-	var barW = (jQuery(this).width() / options.data.length- 15);
-    }
+    } else if (options.style == "vertical") {
+        for (var i=0;i<options.data.length;i++) {
+            percentArray[i] = (options.data[i] / dataMax) * jQuery(this).height();
+        }
+	var barW = (jQuery(this).width() / options.data.length - 5);
+	for (var i=0;i<options.data.length;i++) {
+	    jQuery(this).append("<div class='vbar-chart-bar' id='vbar-"+i+"'></div>");
+	    jQuery(this).children("#vbar-"+i).width(barW);
+	    jQuery(this).children("#vbar-"+i).height(percentArray[i]);
+	    if (i in correctArray) {
+		jQuery(this).children("#vbar-"+i).css('background-color',options.colors[0]);
+	    } else {
+		jQuery(this).children("#vbar-"+i).css('background-color',options.colors[1]);
+	    }
+	    if (options.barStyle == 'fancy') {
+		jQuery(this).children("#vbar-"+i).css("border-top-left-radius",barW/4);
+		jQuery(this).children("#vbar-"+i).css("border-top-right-radius",barW/4);
+	    }
+
+	    if (options.labelStyle == 'text') {
+		if (options.labelDisplay == 'static') {
+		    jQuery(this).children("#vbar-"+i).append("<div class='vbar-chart-bar-label' id='vlabel-"+i+"'>"+options.labels[i]+"<div>");
+		} else if (options.labelDisplay == 'hover') {
+		    jQuery(this).children("#vbar-"+i).attr('title',options.labels[i]);
+		} else if (options.labelDisplay == 'scale') {
+		    jQuery(this).children("#vbar-"+i).append("<div class='vbar-chart-bar-label' id='vlabel-"+i+"'>"+options.labels[i]+"<div>");
+		    jQuery(this).children("#vlabel-"+i).css('font-size',barW/3);
+		} else {
+		    console.log("labelDisplay with value '"+options.labelDisplay+"' is meaningless");
+		}
+	    } else if (options.labelStyle == 'value') {
+		if (options.labelDisplay == 'static') {
+		    jQuery(this).children("#vbar-"+i).append("<div class='vbar-chart-bar-label' id='vlabel-"+i+"'>"+options.data[i]+"<div>");
+		} else if (options.labelDisplay == 'hover') {
+		    jQuery(this).children("#vbar-"+i).attr('title',options.data[i]);
+		} else if (options.labelDisplay == 'scale') {
+		    jQuery(this).children("#vbar-"+i).append("<div class='vbar-chart-bar-label' id='vlabel-"+i+"'>"+options.data[i]+"<div>");
+		    jQuery(this).children("#vlabel-"+i).css('font-size',barW/3);
+		} else {
+		    console.log("labelDisplay with value '"+options.labelDisplay+"' is meaningless");
+		}
+	    } else if (options.labelStyle == 'full') {
+		if (options.labelDisplay == 'static') {
+		    jQuery(this).children("#vbar-"+i).append("<div class='vbar-chart-bar-label' id='vlabel-"+i+"'>"+options.labels[i]+" ("+options.data[i]+")<div>");
+		} else if (options.labelDisplay == 'hover') {
+		    jQuery(this).children("#vbar-"+i).attr('title',options.labels[i]+" ("+options.data[i]+")");
+		} else if (options.labelDisplay == 'scale') {
+		    jQuery(this).children("#vbar-"+i).append("<div class='vbar-chart-bar-label' id='vlabel-"+i+"'>"+options.labels[i]+"("+options.data[i]+")<div>");
+		    jQuery(this).children("#vlabel-"+i).css('font-size',barW/3);
+		} else {
+		    console.log("labelDisplay with value '"+options.labelDisplay+"' is meaningless");
+		}
+
+	    } else {
+		console.log("labelStyle with value'"+options.labelStyle+"' is meaningless");
+	    }
+	}
+    } 
 };
 
